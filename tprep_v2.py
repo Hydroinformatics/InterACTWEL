8c4d44de6a6c08bbceac616345078c18e33ed3ee
Hydroinformatics <Hydroinformatics@users.noreply.github.com>
2018-07-26 11:49:21 -0700

    Add files via upload

--- tprep_v2.py
+++ tprep_v2.py
@@ -1,4 +1,4 @@
-import pandas as pd
+#import pandas as pd
 import numpy as np
 import argparse
 
@@ -11,13 +11,15 @@ if __name__ == '__main__':
                     help='Path to file with raw temperature data')
     parser.add_argument('range', metavar='-p', type=str, nargs='+',
                     help='Starting date (dd/mm/yyyy)')
-    args = parser.parse_args()
-    print args[0]
-    print args[1]
     
-    tin = args[0]
-    new_headers = ['max','min',]
-    mm = pd.read_csv(tin,index_col=0, skiprows=1,names=new_headers)
-    mm.head()
-    #int("Temperature data ")
-    print(mm)
+    args = parser.parse_args()
+    
+    print args.path[0]
+    print args.range[0]
+    
+#    tin = args[0]
+#    new_headers = ['max','min',]
+#    mm = pd.read_csv(tin,index_col=0, skiprows=1,names=new_headers)
+#    mm.head()
+#    #int("Temperature data ")
+#    print(mm)
