c10a766152d4926192873f38c400208441dc3c8a
Hydroinformatics <Hydroinformatics@users.noreply.github.com>
2019-08-16 09:04:09 -0700

    Create ebb_reu_mauga_SWAT_Model.py

---
+++ colab_scripts/ebb_reu_mauga_SWAT_Model.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os, argparse
+os.chdir('D:\Nick\Documents\GitHub\InterACTWEL\src')
+
+from tools.sensitivity.Sensitivity_Analysis_SWAT12_NG import SensitivityAnalysis
+
+
+#%%
+if __name__ == '__main__':
+
+    ##%% Parse Path to Zip file, Uzip and run SWAT Baseline model
+    parser = argparse.ArgumentParser(description='Inputs File')
+    parser.add_argument('path', metavar='-p', type=str, nargs='+',
+                        help='Path to text file with list of input Files')
+    args = parser.parse_args()
+    #%%
+    #input_files = '..\data\Sensitivity_SWAT12\SWAT12_Input_Files.txt'
+    input_files = args.path[0].replace('\\','/')
+    
+    for i in range(1,2):
+        sensitivity = SensitivityAnalysis(input_files)
+        sensitivity.outputcsv = 1
+        sensitivity.inputcsv = 1
+    
+        sensitivity.swat_exe = 'swat_debug32.exe'
+        sensitivity.RunAnalysis(i)
+
