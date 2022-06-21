acca22bf3d1174b48893108e03c20d016b063139
Hydroinformatics <sammy.rivera@oregonstate.edu>
2022-06-21 08:37:17 -0700

    Update OpenMDAO_Dummy_Multiple_Basinsv6.py

--- OpenMDAO/OpenMDAO_Dummy_Multiple_Basinsv6.py
+++ OpenMDAO/OpenMDAO_Dummy_Multiple_Basinsv6.py
@@ -9,20 +9,20 @@ prob = om.Problem()
 prob.model = FEWNexus()
 prob.model.nactors = 2
 
-# prob.driver = om.ScipyOptimizeDriver()
-# prob.driver.options['optimizer'] = "differential_evolution"
-# prob.driver.options['maxiter'] = 100
-# prob.driver.options['tol'] = 1e-8
+prob.driver = om.ScipyOptimizeDriver()
+prob.driver.options['optimizer'] = "differential_evolution"
+prob.driver.options['maxiter'] = 20
+prob.driver.options['tol'] = 1e-8
 
-prob.driver = om.SimpleGADriver()
-prob.driver.options['max_gen'] = 100
-prob.driver.options['pop_size'] = 20
-prob.driver.options['penalty_parameter'] = 200.
-prob.driver.options['penalty_exponent'] = 5.
-prob.driver.options['compute_pareto'] = True
+# prob.driver = om.SimpleGADriver()
+# prob.driver.options['max_gen'] = 10
+# prob.driver.options['pop_size'] = 20
+# prob.driver.options['penalty_parameter'] = 200.
+# prob.driver.options['penalty_exponent'] = 5.
+# prob.driver.options['compute_pareto'] = True
 
 prob.model.add_design_var('wr_vols', lower=0, upper=100)
-prob.model.add_objective('profit', scaler=-1)
+prob.model.add_objective('profit', scaler=1)
 
 #prob.model.add_constraint('aggregator.gw_wrvols', upper = 20.0)
 
@@ -42,7 +42,9 @@ print(prob.get_val('wr_vols'))
 for i in range(0,2):
     print(prob.get_val('farmer_' + str(i+1) + '_plan.indv_profit'))
     exec("parr = prob.model.farmer_" + str(i+1) + "_plan.prob.model")
-    print(parr.get_val('farmer.hru_irr'),parr.get_val('farmer.indv_profit'))
+    #print(parr.get_val('farmer.hru_irr'),parr.get_val('farmer.indv_profit'))
+    print(parr.get_val('farmer.hru_irr'),parr.get_val('farmer.hru_fert'))
+
 
 # desvar_nd = prob.driver.desvar_nd
 # nd_obj = prob.driver.obj_nd
