e738bf1e1cfb7b783f0d3dc7483d695eba1ae918
Hydroinformatics <sammy.rivera@oregonstate.edu>
2022-07-18 12:53:34 -0700

    Update Dummy_SWAT_modelv6.py

--- OpenMDAO/Dummy_SWAT_modelv6.py
+++ OpenMDAO/Dummy_SWAT_modelv6.py
@@ -147,11 +147,12 @@ class FarmerOpt(om.ExplicitComponent):
         #params.add_output('wr_vol', val=1.)
         
         des_vars = p.model.add_subsystem('des_vars', om.IndepVarComp(), promotes=['*'])
-        des_vars.add_output('hru_irr', val=np.ones(len(self.hrus_areas)))
+        inper = np.floor(100.0/len(self.hrus_areas))
+        des_vars.add_output('hru_irr', val=np.ones(len(self.hrus_areas))*inper)
         des_vars.add_discrete_output('hru_fert', val=np.zeros(len(self.hrus_areas)).astype('int'))
         
         p.model.add_subsystem('farmer', Farmer())
-        #p.model.add_subsystem('con_cmp2', om.ExecComp('con2 = sum(hru_irr)'), promotes=['con2'])
+        # p.model.add_subsystem('con_cmp2', om.ExecComp('con2 = sum(hru_irr)'), promotes=['con2'])
         
         p.model.farmer.hrus_areas = self.hrus_areas
         p.model.farmer.hrus_crops = self.hrus_crops
@@ -172,10 +173,12 @@ class FarmerOpt(om.ExplicitComponent):
         
         p.model.add_objective('farmer.indv_profit', scaler=-1)
         p.model.add_objective('farmer.indv_envir', scaler=1)
-        #p.model.add_constraint('con2', lower=100., upper=100)
+        
+        p.model.add_constraint('farmer.const_per', upper=100)
         
         p.setup()
         p.final_setup()
+        
     
     def compute(self, inputs, outputs):
         
@@ -218,17 +221,27 @@ class FarmerOpt(om.ExplicitComponent):
         # outputs['indv_envir'] = sorted_obj[ggr.argsort()[0]][1]
         
         nonzero_ind = np.where(abs(obj_nd[:,0])>0)
-        outputs['indv_profit'] = np.mean(obj_nd[nonzero_ind,0])
+        if len(nonzero_ind[0]) > 0:
+            outputs['indv_profit'] = np.mean(obj_nd[nonzero_ind,0])
+        else:
+            outputs['indv_profit'] = 0.
+            
         nonzero_ind = np.where(abs(obj_nd[:,1])>0)
-        outputs['indv_envir'] = np.mean(obj_nd[nonzero_ind,1])
+        if len(nonzero_ind[0]) > 0:
+            outputs['indv_envir'] = np.mean(obj_nd[nonzero_ind,1])
+        else:
+            #outputs['indv_envir'] = float('inf')
+            outputs['indv_envir'] = 0.
+        
+        # with open("farmer_" + str(self.farmer_id) + "_res.txt", "a") as myfile:
+        #     nonzero_ind = np.where(abs(obj_nd[:,0])>0)[0]
+        #     #for i in range(0,len(obj_nd[:,0])):
+        #     for i in nonzero_ind:
+        #         myfile.write(str(int(inputs['wr_vol'][0])) + ', ' + str(obj_nd[i,0]) + ', ' + str(obj_nd[i,1]) + ', ' + str(p.driver.desvar_nd[i]) + '\n')
         
         
-        with open("farmer_" + str(self.farmer_id) + "_res.txt", "a") as myfile:
-            nonzero_ind = np.where(abs(obj_nd[:,0])>0)[0]
-            #for i in range(0,len(obj_nd[:,0])):
-            for i in nonzero_ind:
-                myfile.write(str(int(inputs['wr_vol'][0])) + ', ' + str(obj_nd[i,0]) + ', ' + str(obj_nd[i,1]) + ', ' + str(p.driver.desvar_nd[i]) + '\n')
-        
+        if str(outputs['indv_profit'][0]) == 'nan':
+            stp = 0
         #sorted_obj = obj_nd[obj_nd[:, 1].argsort()]
         
         #outputs['indv_decisions'] = p.driver.desvar_nd
@@ -284,13 +297,14 @@ class Farmer(om.ExplicitComponent):
     def setup(self):
         
         #self.add_input('wr_vol', val=0.0)
-        self.add_input('hru_irr', val=np.ones(len(self.hrus_areas)))
+        inper = np.floor(100.0/len(self.hrus_areas))
+        self.add_input('hru_irr', val=np.ones(len(self.hrus_areas))*inper)
         self.add_discrete_input('hru_fert', val=np.zeros(len(self.hrus_areas)).astype('int'))
 
         self.add_output('indv_profit', val=0.0)
         self.add_output('indv_costs', val=0.0)
         self.add_output('indv_envir', val=0.0)
-        #self.add_output('indv_obj', val=0.0)
+        self.add_output('const_per', val=0.0)
         self.add_output('indv_crops_yields', val=np.zeros(len(self.crops_price)))
         
     def setup_partials(self):
@@ -310,37 +324,40 @@ class Farmer(om.ExplicitComponent):
         indv_envir = 0
         indv_costs = 0
 
-        if (sum(inputs['hru_irr']) - 100.0) > 0.:
-            #outputs['indv_profit'] = - 5000000000
-            outputs['indv_profit'] = 0.
-            outputs['indv_costs'] = 0.
-            outputs['indv_envir'] = 0.
-            outputs['indv_crops_yields'] = np.zeros(len(self.crops_price))
+        # if (sum(inputs['hru_irr']) - 100.0) > 0.:
+        #     #outputs['indv_profit'] = - 5000000000
+        #     outputs['indv_profit'] = 0.
+        #     outputs['indv_costs'] = 0.
+        #     #outputs['indv_envir'] = float('inf') 
+        #     outputs['indv_envir'] = 0.
+        #     outputs['indv_crops_yields'] = np.zeros(len(self.crops_price))
             
-            #outputs['total_irr'] = total_irr
-            #outputs['indv_profit'] = - abs((sum(inputs['hru_irr']) - 100.0))*100000000
-        else:
-            for i in range(0,len(self.hrus_areas)):
+        #     #outputs['total_irr'] = total_irr
+        #     #outputs['indv_profit'] = - abs((sum(inputs['hru_irr']) - 100.0))*100000000
+        # else:
+            
+        for i in range(0,len(self.hrus_areas)):
 
-                irr_amt = (inputs['hru_irr'][i]/100.)*wr_vol
-                crop_yield = np.interp(irr_amt, self.w_crops[:,0], self.w_crops[:,self.hrus_crops[i]]) 
-                cost_f = np.interp(irr_amt, range(0,101), self.cost_fert[:,discrete_inputs['hru_fert'][i]])*self.hrus_areas[i]
+            irr_amt = (inputs['hru_irr'][i]/100.)*wr_vol
+            crop_yield = np.interp(irr_amt, self.w_crops[:,0], self.w_crops[:,self.hrus_crops[i]]) 
+            cost_f = np.interp(irr_amt, range(0,101), self.cost_fert[:,discrete_inputs['hru_fert'][i]])*self.hrus_areas[i]
+        
+            per_yield = 1 + (0.8*-np.exp(self.p_crops_a[self.hrus_crops[i]-1]*irr_amt))
+            profit = crop_yield*per_yield*self.hrus_areas[i]*self.crops_price[self.hrus_crops[i]-1] - cost_f # profit function        
             
-                per_yield = 1 + (0.8*-np.exp(self.p_crops_a[self.hrus_crops[i]-1]*irr_amt))
-                profit = crop_yield*per_yield*self.hrus_areas[i]*self.crops_price[self.hrus_crops[i]-1] - cost_f # profit function        
-                
-                envir = (1 + (0.8*-np.exp(self.p_crops_a[self.hrus_crops[i]-1]*irr_amt)))*self.p_crops_b[discrete_inputs['hru_fert'][i]]
-                
-                indv_profit = indv_profit + profit
-                indv_costs = indv_costs + cost_f
-                indv_envir  = indv_envir + envir 
-                #total_irr = total_irr + irr_amt
-                
-                outputs['indv_crops_yields'][self.hrus_crops[i]-1] = outputs['indv_crops_yields'][self.hrus_crops[i]-1] + crop_yield*per_yield*self.hrus_areas[i]
-             
-            outputs['indv_envir'] = indv_envir
-            outputs['indv_profit'] = indv_profit    
-            outputs['indv_costs'] = indv_costs
+            envir = (1 + (0.8*-np.exp(self.p_crops_a[self.hrus_crops[i]-1]*irr_amt)))*self.p_crops_b[discrete_inputs['hru_fert'][i]]
+            
+            indv_profit = indv_profit + profit
+            indv_costs = indv_costs + cost_f
+            indv_envir  = indv_envir + envir 
+            #total_irr = total_irr + irr_amt
+            
+            outputs['indv_crops_yields'][self.hrus_crops[i]-1] = outputs['indv_crops_yields'][self.hrus_crops[i]-1] + crop_yield*per_yield*self.hrus_areas[i]
+         
+        outputs['indv_envir'] = indv_envir
+        outputs['indv_profit'] = indv_profit    
+        outputs['indv_costs'] = indv_costs
+        outputs['const_per'] = sum(inputs['hru_irr'])
             
         #print('OPT_Farmer_' + str(self.farmer_id) + ': ' + str(wr_vol) + ', ' + str(inputs['hru_irr']) + ', ' + str(outputs['indv_profit']))
 
