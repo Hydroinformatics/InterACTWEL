b23f8be0a267786815c3e3d19dfb33080926add5
Samuel Rivera <sammy.rivera14@gmail.com>
2023-03-20 18:04:17 -0700

    Update Sensitivity_WRsScenario_SWAT15.py

--- OpenMDAO/Sensitivity_WRsScenario_SWAT15.py
+++ OpenMDAO/Sensitivity_WRsScenario_SWAT15.py
@@ -7,6 +7,10 @@ import matplotlib.pyplot as plt
 import pyodbc
 import shapefile
 
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import LineString
+
 #%%
 def FindHRUID(model_path):
     print(model_path)
@@ -247,6 +251,41 @@ for i in range(0,len(hru_portMorrow_records)):
     hruid_portMorrow.append((hru_portMorrow_records[i][6], hrugis_dict[hru_portMorrow_records[i][6]]))
 hruid_portMorrow = np.asarray(hruid_portMorrow)
 
+
+data_path = r'C:\Users\riversam\Box\Research\SWAT\SWAT_JetStream_runs'
+
+crs = 'EPSG:26911'
+subs = gpd.read_file(data_path + "/subs1.shp")
+subs.to_crs(crs, inplace=True)
+
+hrus2 = gpd.read_file(data_path + "/hru2.shp")
+hrus2.to_crs(crs, inplace=True)
+
+nowa = gpd.read_file(data_path + "/NOWA_hrus_pumping_limitsv3.shp")
+nowa.to_crs(crs, inplace=True)
+
+# subs['g1'] = subs.geometry
+# nowa['g2'] = nowa.geometry
+# gdf_joined = subs.sjoin(nowa, how="inner")
+# gdf_joined['area_joined'] = gdf_joined.area
+# gdf_joined['sub_area'] = (gdf_joined['area_joined'] / gdf_joined['Shape_Area'])
+
+# def f(frame):
+#     """A function to calculate overlap percentage"""
+#     interpct = 100*frame.g1.intersection(frame.g2).area/frame.g1.area
+#     return interpct
+
+# gdf_joined['pct'] = gdf_joined.apply(f, axis=1)
+
+# #%%
+# subs_nowa = [int(row['Subbasin']) for i, row in gdf_joined.iterrows() if row['pct'] >= 10]
+
+# fig, ax = plt.subplots()
+# subs.plot(ax=ax, facecolor='none', edgecolor = 'black', lw=0.7)
+# nowa.plot(ax=ax, facecolor='yellow', edgecolor = 'black', alpha=0.7)
+# for sid in subs_nowa:
+#     subs.loc[subs['Subbasin']==sid,'geometry'].plot(ax=ax, facecolor='blue', alpha=0.5)
+
 #%%
 mtg_file = r'C:\Users\riversam\Box\Research\SWAT\QSWAT\Backup_v4_Iter15_bestsim\Backup'
 fnames = os.listdir(mtg_file)
@@ -301,7 +340,8 @@ final_wr_list = np.delete(final_wr_list, np.where(final_wr_list == 9999))
 
 # if build_model == 1:
 
-for iterrun in range(0,len(final_wr_list)+1):
+for iterrun in range(0,1):
+#for iterrun in range(0,len(final_wr_list)+1):
 #     print "Creating Scenarios"
     
 #     hru_irr = [] # This section needs to be automated: using output.std
@@ -334,7 +374,7 @@ for iterrun in range(0,len(final_wr_list)+1):
     cr_wrdist = dict()
     
     #temp_path = cluster_path + '/ITER_' + str(iterrun)
-    temp_path = cluster_path + '/ITERS'
+    temp_path = cluster_path + '/ITERS_LOW'
     
     isExist = os.path.exists(temp_path)
     if not isExist:
@@ -380,12 +420,14 @@ for iterrun in range(0,len(final_wr_list)+1):
 #             with open(outpath + '/CR_wr_dist_' + str(itern) + '.json', 'w+') as fp:
 #                 json.dump(cr_wrdist, fp)        
            
-    txt_file = temp_path + '/wrdata_' + str(iterrun) + '.dat'
+    #txt_file = temp_path + '/wrdata_' + str(iterrun) + '.dat'
+    txt_file = temp_path + '/wrdata_cr' + '.dat'
     
     file_org = open(file_path, 'r')
     Lines = file_org.readlines()
     
     filein = open(txt_file,'w+')
+    
     
     for line in Lines:
         linesplit = line.split()
@@ -402,9 +444,11 @@ for iterrun in range(0,len(final_wr_list)+1):
                     atxt = atxt + str(int(linesplit[3])).rjust(6) + ''.rjust(3)
                 else:
                     atxt = atxt + str(999999).rjust(6) + ''.rjust(3)
+                    # atxt = atxt + str(0).rjust(6) + ''.rjust(3)
             else:
                 if int(linesplit[1]) == final_wr_list[iterrun-1]:
                     atxt = atxt + str(999999).rjust(6) + ''.rjust(3)
+                    # atxt = atxt + str(0).rjust(6) + ''.rjust(3)
                 else:
                     atxt = atxt + str(int(linesplit[3])).rjust(6) + ''.rjust(3)
                     
@@ -419,6 +463,44 @@ for iterrun in range(0,len(final_wr_list)+1):
     
     file_org.close()
     filein.close()
+    
+    # for line in Lines:
+    #     linesplit = line.split()
+    #     if 'year' not in linesplit[0].lower():
+    #         atxt = str(int(linesplit[0])).rjust(4) + ''.rjust(3)
+    #         atxt = atxt + str(int(linesplit[1])).rjust(5) + ''.rjust(3)
+    #         atxt = atxt + str(int(linesplit[2])).rjust(4) + ''.rjust(3)
+            
+    #         #432082
+    #         #atxt = atxt + str(int(linesplit[3])).rjust(6) + ''.rjust(3)
+            
+    #         if iterrun == 0:
+    #             if int(linesplit[1]) == 9999:
+    #                 atxt = atxt + str(int(linesplit[3])).rjust(6) + ''.rjust(3)
+    #             else:
+    #                 #atxt = atxt + str(999999).rjust(6) + ''.rjust(3)
+    #                 if int(linesplit[1]) == 1791 or int(linesplit[1]) == 1792 or int(linesplit[1]) == 1793:
+    #                     atxt = atxt + str(0).rjust(6) + ''.rjust(3)
+    #                 else:
+    #                     atxt = atxt + str(int(linesplit[3])).rjust(6) + ''.rjust(3)
+    #         else:
+    #             if int(linesplit[1]) == final_wr_list[iterrun-1]:
+    #                 #atxt = atxt + str(999999).rjust(6) + ''.rjust(3)
+    #                 atxt = atxt + str(0).rjust(6) + ''.rjust(3)
+    #             else:
+    #                 atxt = atxt + str(int(linesplit[3])).rjust(6) + ''.rjust(3)
+                    
+            
+    #         atxt = atxt + str(int(linesplit[4])).rjust(4) + ''.rjust(3)
+    #         atxt = atxt + str(int(linesplit[5])).rjust(4)
+    #         filein.write(atxt + '\n') 
+            
+    #     else:
+    #         filein.write(line) 
+    
+    
+    # file_org.close()
+    # filein.close()
     
 
     
